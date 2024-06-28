377f7f38355e32face30fb7fe6db9b17a84a4cdc
xrisent <ormonov31261@gmail.com>
2024-06-28 16:47:50 +0600

    made shop page and adapting for main page

---
+++ pages/main_page/js.js
@@ -0,0 +1,7 @@
+let ul = document.querySelector('header ul')
+let burger = document.querySelector('.burger')
+
+let ShowBurger = () =>{
+    ul.classList.toggle('active')
+    burger.classList.toggle('active')
+}
